dd8abbafc1b1d6645b4ad1356bded7a12872db36
Zedii <47713084+Zediyo@users.noreply.github.com>
2022-10-31 21:51:01 +0200

    Update health_check.sh

--- health_check.sh
+++ health_check.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 response=$(curl -s https://crimson-violet-5457.fly.dev/health)
-expected = "ok"
 
-if [ $response = $expected ]
+if [ "$response" == "ok" ];
   then
     exit 0
-  else
-    exit 1
 fi
+
+exit 1
