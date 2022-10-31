af1e1f31e3e0d0a5935d66c523a3e8979219d724
Zedii <jzedlf@gmail.com>
2022-10-31 20:21:02 +0200

    test2

--- app.js
+++ app.js
@@ -6,13 +6,12 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.get('/health', () => {
-  throw 'error...'
-  //res.send('ok')
+app.get('/health', (req, res) => {
+  res.send('ok')
 })
 
 app.get('/version', (req, res) => {
-  res.send('2')
+  res.send('3')
 })
 
 app.listen(PORT, () => {
